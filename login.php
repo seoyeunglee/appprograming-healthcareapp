8935ed06b687aa587555def75846ba16079685d8
seoyeunglee <syoo0183oo@gmail.com>
2023-12-04 16:19:05 +0900

    php

--- login.php
+++ login.php
@@ -6,8 +6,8 @@ $password = "1234"; // 데이터베이스 비밀번호
 $dbname = "test"; // 데이터베이스 이름
 
 // POST로 전송된 데이터 받기
-$userId = $_POST['userId'];
-$userPw = $_POST['userPw'];
+$loginId = $_POST['loginId'];
+$loginPw = $_POST['loginPw'];
 
 // 데이터베이스 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -18,10 +18,10 @@ if ($conn->connect_error) {
 }
 
 // 비밀번호 해싱 (추가 보안을 위해 사용자 비밀번호를 해싱하여 저장하는 것이 좋습니다.)
-$hashed_userPw = password_hash($userPw, PASSWORD_DEFAULT);
+$hashed_loginPw = password_hash($loginPw, PASSWORD_DEFAULT);
 
 // SQL 쿼리 작성 및 실행
-$sql = "INSERT INTO Login (userId, userPw) VALUES ('$userId', '$hashed_userPw')";
+$sql = "INSERT INTO Login (loginId, loginPw) VALUES ('$loginId', '$hashed_loginPw')";
 
 if ($conn->query($sql) === TRUE) {
     echo "로그인이 완료되었습니다.";
